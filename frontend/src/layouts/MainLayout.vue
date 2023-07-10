edaf975c8fb36870feb6827d4c21b5105ad5ae98
RobinCoblentz <82441417+RobinCoblentz@users.noreply.github.com>
2023-07-10 12:05:46 +0200

    init frontend

---
+++ frontend/src/layouts/MainLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="q-pa-md">
+    <q-layout style="margin: 0px; height: 100%">
+      <q-page-container>
+        <q-page class="q-pa-md" style="margin: 0px; height: 100%">
+          <router-view />
+        </q-page>
+      </q-page-container>
+    </q-layout>
+  </div>
+</template>
+
+<script>
+// import NavbarComponants from "../components/NavbarComponants.vue";
+
+export default {
+  name: "MainLayout",
+  components: {},
+  setup() {
+    return {};
+  },
+};
+</script>
---
+++ frontend/src/pages/HomePage.vue
@@ -0,0 +1,55 @@
+<template>
+  <div><h3>Calories calculator</h3></div>
+  <div class="q-pa-md" style="max-width: 400px">
+    <q-form @save="onSubmit" @analyze="onAnalyze" class="q-gutter-md">
+      <q-input v-model="text" filled type="textarea" />
+      <div>
+        <q-btn
+          label="Enregistrer"
+          type="save"
+          color="primary"
+          style="margin: 5px"
+        />
+        <q-btn
+          label="Analyser"
+          type="analyze"
+          color="primary"
+          style="margin: 5px"
+        />
+      </div>
+    </q-form>
+  </div>
+  <div><h4>Rapport d'analyse</h4></div>
+</template>
+
+<script>
+import { useQuasar } from "quasar";
+import { ref } from "vue";
+
+// import CardComponents from "../components/CardComponents.vue";
+
+export default {
+  name: "HomePage",
+  components: {},
+  setup() {
+    return {
+      text: ref(""),
+
+      onSubmit() {
+        console.log("submit");
+      },
+
+      onAnalyze() {
+        console.log("analyze");
+        const box = `
+        <div id='box'>
+          <button id='button-1'>Button</button>
+        </div>`;
+
+        document.body.innerHTML = box;
+      },
+    };
+  },
+};
+</script>
+<style></style>
