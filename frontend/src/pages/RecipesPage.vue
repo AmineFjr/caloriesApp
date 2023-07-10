7d96e18856d77f74531949a4bfe9107a2ff56175
Nadia <nschwaller@tiz.fr>
2023-07-10 15:15:20 +0200

    Mise en place de la carde (pas de duplication de card par donnée bdd)

--- frontend/src/pages/RecipesPage.vue
+++ frontend/src/pages/RecipesPage.vue
@@ -1,15 +1,34 @@
 <template>
-  <div class="fullscreen flex flex-center">
-    <div>
-      <div style="font-size: 10vh">RecipesPage</div>
-    </div>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-card class="my-card bg-teal-6 text-white">
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-card-section>
+        {{ lorem }}
+      </q-card-section>
+
+      <q-separator dark />
+
+      <q-card-actions>
+        <q-btn flat>Action 1</q-btn>
+        <q-btn flat>Action 2</q-btn>
+      </q-card-actions>
+    </q-card>
   </div>
 </template>
 
 <script>
-import { defineComponent } from "vue";
 
-export default defineComponent({
-  name: "RecipesPage",
-});
+export default {
+  name: "RecipePage",
+
+  setup() {
+    return {
+    };
+  },
+};
 </script>
+<style></style>
