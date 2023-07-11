7c9383bb47255b244a42af7a9af72ed8d5d77dbb
Nalvac <vacna1997@gmail.com>
2023-07-11 17:21:13 +0200

    Find recipe by key

--- backend/controllers/recipeController.js
+++ backend/controllers/recipeController.js
@@ -16,11 +16,8 @@ async function getAllRecipes(req, res){
 // Contrôleur pour récupérer une recette par son identifiant
 async function getRecipeById (req, res) {
     try {
-        const { recipeId } = req.params.id;
-        console.log(recipeId)
-
         // Recherche de la recette par son identifiant
-        const recipe = await recipeModel.findByPk(recipeId, {
+        const recipe = await recipeModel.findByPk(req.params.id, {
             include: 'ingredients' // Inclure les ingrédients liés à la recette
         });
 
